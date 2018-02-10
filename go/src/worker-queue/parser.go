195efe7c58f42f734d2287b6dbc2515fc98c49ea
alainfonhof <alainfh94@gmail.com>
2018-02-11 00:36:03 +0100

    Start k-ary to binary recursion still lot of type bugs

--- go/src/worker-queue/parser.go
+++ go/src/worker-queue/parser.go
@@ -20,22 +20,36 @@ func parseJson(jsonInput string) ([]interface{}, string) {
 /*
 [map[then:[map[conditions:[map[quoteCurrency:OMG timeframeInMS:3.6e+06 baseMetric:price value:20 conditionType:percentage-increase baseCurrency:ETH] map[quoteCurrency:OMG timeframeInMS:3.6e+06 baseMetric:price quoteMetric:boilinger-band-upper-bound value:20 conditionType:percentage-increase baseCurrency:ETH]] action:map[quoteCurrency:OMG quantity:100 value:0.012 orderType:limit-buy orderValueType:absolute baseCurrency:ETH] priority:0] map[conditions:[map[quoteCurrency:OMG timeframeInMS:7.2e+06 baseMetric:rsi value:5 conditionType:absolute-increase baseCurrency:ETH]] action:map[quoteCurrency:OMG quantity:100 value:0.012 orderType:limit-sell orderValueType:absolute baseCurrency:ETH] priority:1]] priority:0 conditions:[map[conditionType:percentage-increase baseCurrency:ETH quoteCurrency:OMG timeframeInMS:3.6e+06 baseMetric:price value:20]] action:map[quoteCurrency:OMG quantity:100 value:0.012 orderType:limit-buy orderValueType:absolute baseCurrency:ETH]] map[conditions:[map[quoteCurrency:OMG timeframeInMS:7.2e+06 baseMetric:rsi value:5 conditionType:absolute-increase baseCurrency:ETH]] action:map[quoteCurrency:OMG quantity:100 value:0.012 orderType:limit-buy orderValueType:absolute baseCurrency:ETH] priority:1] map[priority:2 conditions:[map[timeframeInMS:7.2e+06 baseMetric:rsi value:10 conditionType:absolute-decrease baseCurrency:ETH quoteCurrency:OMG]] action:map[orderValueType:absolute baseCurrency:ETH quoteCurrency:OMG quantity:100 value:0.012 orderType:limit-sell]]]
 */
-func parseBinaryTree(tree []interface{}) bool {
-    conditions := tree[0].(map[string]interface{})["conditions"]
-	action := tree[0].(map[string]interface{})["action"]
-    root := Tree{Left: nil, Right: nil, Conditions: createConditionsFromSlice(conditions.([]interface{})), Action: createActionFromMap(action.(map[string]interface {})) }
+func parseBinaryTree(tree []interface{}) Tree {
+    // conditions := tree[0].(map[string]interface{})["conditions"]
+	// action := tree[0].(map[string]interface{})["action"]
+    // root := Tree{Left: nil, Right: nil, Conditions: createConditionsFromSlice(conditions.([]interface{})), Action: createActionFromMap(action.(map[string]interface {})) }
+    root := Tree{ Left: nil, Right: nil, Conditions: []Condition{{ConditionType: "true"}}, Action: Action{} }
     fmt.Println(root)
-	// _parseBinaryTree(tree, &root)
-	return true
+	root.Left = _parseBinaryTree(tree, &root.Left, 0)
+	return root
 }
 
-// func _parseBinaryTree(tree []interface{}, parent *Tree){
-// 	for i, node := range tree {
-// 		newNode := Tree{Left: nil, Right: nil, Conditions: createConditionsFromSlice(tree[i]["conditions"]), Action: createActionFromMap(tree[i]["action"])}
-// 		parent.Left := node
-// 		_parseBinaryTree(tree[i], &node)
-// 	}
-// }
+// https://www.geeksforgeeks.org/construct-complete-binary-tree-given-array/
+func _parseBinaryTree(siblings []interface{}, root *Tree, i int) Tree{
+    if i < len(siblings) {
+        conditions := siblings[i].(map[string]interface{})["conditions"]
+        action := siblings[i].(map[string]interface{})["action"]
+        newNode := Tree{Left: nil, Right: nil, Conditions: createConditionsFromSlice(conditions.([]interface{})), Action: createActionFromMap(action.(map[string]interface {})) }
+        root := newNode
+
+        if then, ok := siblings[i].(map[string]interface{})["then"]; ok {
+            root.Left = _parseBinaryTree(then, &root.Left, 0)
+        } else {
+            root.Left = _parseBinaryTree([]int, &root.Left, 0)
+        }
+
+        tmp := i
+        tmp++
+        root.Right = _parseBinaryTree(siblings[i], &root.Right, tmp)
+    }
+    return root
+}
 
 func createConditionsFromSlice(conditionsSlice []interface{}) []Condition {
 	conditions := []Condition{}
@@ -76,5 +90,6 @@ func main() {
 	if err != "" {
 		fmt.Printf(err)
 	}
-	parseBinaryTree(tree)
+	root := parseBinaryTree(tree)
+    fmt.Println(root)
 }
