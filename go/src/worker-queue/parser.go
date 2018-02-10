c9de810e9b79d35e77c913c11dd5a6375ed090f8
Jelle van Mil <jellevanmil@gmail.com>
2018-02-10 22:26:29 +0100

    Parser adding Condition(s) and Action json data to root node now works

--- go/src/worker-queue/parser.go
+++ go/src/worker-queue/parser.go
@@ -21,10 +21,10 @@ func parseJson(jsonInput string) ([]interface{}, string) {
 [map[then:[map[conditions:[map[quoteCurrency:OMG timeframeInMS:3.6e+06 baseMetric:price value:20 conditionType:percentage-increase baseCurrency:ETH] map[quoteCurrency:OMG timeframeInMS:3.6e+06 baseMetric:price quoteMetric:boilinger-band-upper-bound value:20 conditionType:percentage-increase baseCurrency:ETH]] action:map[quoteCurrency:OMG quantity:100 value:0.012 orderType:limit-buy orderValueType:absolute baseCurrency:ETH] priority:0] map[conditions:[map[quoteCurrency:OMG timeframeInMS:7.2e+06 baseMetric:rsi value:5 conditionType:absolute-increase baseCurrency:ETH]] action:map[quoteCurrency:OMG quantity:100 value:0.012 orderType:limit-sell orderValueType:absolute baseCurrency:ETH] priority:1]] priority:0 conditions:[map[conditionType:percentage-increase baseCurrency:ETH quoteCurrency:OMG timeframeInMS:3.6e+06 baseMetric:price value:20]] action:map[quoteCurrency:OMG quantity:100 value:0.012 orderType:limit-buy orderValueType:absolute baseCurrency:ETH]] map[conditions:[map[quoteCurrency:OMG timeframeInMS:7.2e+06 baseMetric:rsi value:5 conditionType:absolute-increase baseCurrency:ETH]] action:map[quoteCurrency:OMG quantity:100 value:0.012 orderType:limit-buy orderValueType:absolute baseCurrency:ETH] priority:1] map[priority:2 conditions:[map[timeframeInMS:7.2e+06 baseMetric:rsi value:10 conditionType:absolute-decrease baseCurrency:ETH quoteCurrency:OMG]] action:map[orderValueType:absolute baseCurrency:ETH quoteCurrency:OMG quantity:100 value:0.012 orderType:limit-sell]]]
 */
 func parseBinaryTree(tree []interface{}) bool {
-	conditions := tree[0].(map[string]interface{})
-	fmt.Println(conditions["conditions"])
-	root := Tree{Left: nil, Right: nil, Conditions: createConditionsFromSlice(conditions["conditions"]), Action: {} }
-	// fmt.Println(root)
+    conditions := tree[0].(map[string]interface{})["conditions"]
+	action := tree[0].(map[string]interface{})["action"]
+    root := Tree{Left: nil, Right: nil, Conditions: createConditionsFromSlice(conditions), Action: createActionFromMap(action.(map[string]interface {})) }
+    fmt.Println(root)
 	// _parseBinaryTree(tree, &root)
 	return true
 }
@@ -37,10 +37,12 @@ func parseBinaryTree(tree []interface{}) bool {
 // 	}
 // }
 
-func createConditionsFromSlice(s interface{}) []Condition {
-	conditions := make([]Condition, len(s))
-	for i, c := range s {
-		conditions = append(conditions, createConditionFromMap(c))
+func createConditionsFromSlice(conditionsSlice interface{}) []Condition {
+	conditions := []Condition{}
+    cs := conditionsSlice.([]interface{})
+	for i, condition := range cs {
+        _ = i
+		conditions = append(conditions, createConditionFromMap(condition.(map[string]interface {})))
 	}
 	return conditions
 }
@@ -54,14 +56,14 @@ func createConditionFromMap(m map[string]interface{}) Condition {
     return result
 }
 
-// func createActionFromMap(m map[string]interface{}) Action {
-//     var result Action
-//     err := mapstructure.Decode(m, &result)
-//     if err != nil {
-//     	fmt.Printf("Error: %v\n", err)
-//     }
-//     return result
-// }
+func createActionFromMap(m map[string]interface{}) Action {
+    var result Action
+    err := mapstructure.Decode(m, &result)
+    if err != nil {
+    	fmt.Printf("Error: %v\n", err)
+    }
+    return result
+}
 
 func main() {
 	file, e := ioutil.ReadFile("./tree-example.json")
