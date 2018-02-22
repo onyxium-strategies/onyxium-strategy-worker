d1745a4c345388f542a8dbb14bd337bc6115ff99
alainfonhof <alainfh94@gmail.com>
2018-02-22 16:30:44 +0100

    Removed WriteMessage from client side. Now only reads every x seconds. Build in timer that stops the job after a duration.

--- go/src/market-feed-client/main.go
+++ go/src/market-feed-client/main.go
@@ -18,24 +18,33 @@ func main() {
 	flag.Parse()
 	log.SetFlags(0)
 
+	// Enable closing connection with ctrl-c
 	interrupt := make(chan os.Signal, 1)
 	signal.Notify(interrupt, os.Interrupt)
 
 	u := url.URL{Scheme: "ws", Host: *addr, Path: "/echo"}
 	log.Printf("connecting to %s", u.String())
 
-	c, _, err := websocket.DefaultDialer.Dial(u.String(), nil)
+	conn, _, err := websocket.DefaultDialer.Dial(u.String(), nil)
 	if err != nil {
 		log.Fatal("dial:", err)
 	}
-	defer c.Close()
+	defer conn.Close()
 
-	done := make(chan struct{})
+	ticker := time.NewTicker(2 * time.Second).C
+	done := make(chan bool)
 
-	go func() {
-		defer close(done)
-		for {
-			_, message, err := c.ReadMessage()
+	// Close the connection after a duration
+	timer := time.NewTimer(20 * time.Second).C
+	// go func() {
+	// 	time.Sleep(10 * time.Second)
+	// 	done <- true
+	// }()
+
+	for {
+		select {
+		case <-ticker:
+			_, message, err := conn.ReadMessage()
 			if err != nil {
 				log.Println("read:", err)
 				return
@@ -44,28 +53,19 @@ func main() {
 			err = json.Unmarshal(message, &objmap)
 			fmt.Printf("recv: %s", objmap["BTC-LTC"])
 			fmt.Println("\n")
-		}
-	}()
 
-	ticker := time.NewTicker(time.Second)
-	defer ticker.Stop()
-
-	for {
-		select {
-		case <-done:
+		case <-timer:
+			log.Println("timer ended")
 			return
-		case t := <-ticker.C:
-			err := c.WriteMessage(websocket.TextMessage, []byte(t.String()))
-			if err != nil {
-				log.Println("write:", err)
-				return
-			}
+		case <-done:
+			log.Println("job done")
+			return
 		case <-interrupt:
 			log.Println("interrupt")
 
 			// Cleanly close the connection by sending a close message and then
 			// waiting (with timeout) for the server to close the connection.
-			err := c.WriteMessage(websocket.CloseMessage, websocket.FormatCloseMessage(websocket.CloseNormalClosure, ""))
+			err := conn.WriteMessage(websocket.CloseMessage, websocket.FormatCloseMessage(websocket.CloseNormalClosure, ""))
 			if err != nil {
 				log.Println("write close:", err)
 				return
