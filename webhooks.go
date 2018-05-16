ed7b6bf4fc1528b00324f71fe74b47075c9a2eaf
alainfonhof <alainfh94@gmail.com>
2018-05-16 10:43:14 +0200

    Fixed major bug that caused the webhook to not use the correct emoji.

--- webhooks.go
+++ webhooks.go
@@ -6,13 +6,13 @@ import (
 )
 
 func eventUserSignUp() error {
-	body := `{"text":"A new user has just signed up.","icon_emoji":":dancer::skin-tone-2:"}`
+	body := `{"text":"A new user has just signed up.","icon_emoji":":golang:","username":"Go"}`
 	_, err := http.Post("https://hooks.slack.com/services/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", "application/json", bytes.NewBuffer([]byte(body)))
 	return err
 }
 
 func eventUserActivated() error {
-	body := `{"text":"A new user has been activated.","icon_emoji":":dancer::skin-tone-5:"}`
+	body := `{"text":"A new user has been activated.","icon_emoji":":golang:","username":"Go"}`
 	_, err := http.Post("https://hooks.slack.com/services/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", "application/json", bytes.NewBuffer([]byte(body)))
 	return err
 }
