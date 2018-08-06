ecdbe1b1db22052d852974cbd0d34c9bedd9bd05
Jelle van Mil <jellevanmil@gmail.com>
2018-08-07 01:54:28 +0200

    Moved respond helper functions to bottom of file

--- handlers.go
+++ handlers.go
@@ -9,18 +9,6 @@ import (
 	"net/url"
 )
 
-func respondWithError(w http.ResponseWriter, code int, message string) {
-	respondWithJSON(w, code, map[string]string{"error": message})
-}
-
-func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
-	response, _ := json.Marshal(payload)
-
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(code)
-	w.Write(response)
-}
-
 func UserAll(w http.ResponseWriter, r *http.Request) {
 	users, err := env.DataStore.UserAll()
 	if err != nil {
@@ -131,3 +119,16 @@ func EmailConfirm(w http.ResponseWriter, r *http.Request) {
 	}
 	respondWithJSON(w, http.StatusOK, response)
 }
+
+func respondWithError(w http.ResponseWriter, code int, message string) {
+	respondWithJSON(w, code, map[string]string{"error": message})
+}
+
+func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
+	response, _ := json.Marshal(payload)
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(code)
+	w.Write(response)
+}
+
