3c7ef1ee01f4734681e5ee0c08b8757c367b8554
vanmil <jellevanmil@gmail.com>
2019-07-20 12:48:21 +0200

    Update webhooks.go
    Deleted old unavailable webhook

--- webhooks.go
+++ webhooks.go
@@ -7,12 +7,12 @@ import (
 
 func eventUserSignUp() error {
 	body := `{"text":"A new user has just signed up.","icon_emoji":":golang:","username":"Go"}`
-	_, err := http.Post("https://hooks.slack.com/services/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", "application/json", bytes.NewBuffer([]byte(body)))
+	_, err := http.Post("", "application/json", bytes.NewBuffer([]byte(body)))
 	return err
 }
 
 func eventUserActivated() error {
 	body := `{"text":"A new user has been activated.","icon_emoji":":golang:","username":"Go"}`
-	_, err := http.Post("https://hooks.slack.com/services/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", "application/json", bytes.NewBuffer([]byte(body)))
+	_, err := http.Post("", "application/json", bytes.NewBuffer([]byte(body)))
 	return err
 }
